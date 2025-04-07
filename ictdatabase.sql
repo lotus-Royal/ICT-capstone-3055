3e9398c4d4c1c57e8e330499a64513c2dd91d698
lotus royal <shahi024kamal@gmail.com>
2025-04-07 13:25:48 +1000

    Add files via upload

---
+++ ictdatabase.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ICTCAP;
+  idnum int(20) auto_increment primary key,
+  fullname varchar(255),
+  birthday date(25),
+  gender1 varchar(25),
+  gender2 varchar(25),
+  contact int(25),
+  email varchar(255),
+  confirm varchar(255)
